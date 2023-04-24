2941e9b8d95869dd0d982a0af8587a18b317ae43
Zesting <pp950209@naver.com>
2023-04-24 18:22:18 +0900

    board_commit

--- src/main/java/ezenstudy/bts/controller/BoardController.java
+++ src/main/java/ezenstudy/bts/controller/BoardController.java
@@ -17,36 +17,39 @@ import ezenstudy.bts.service.ReviewBoardService;
 @Controller
 public class BoardController {
     private final ReviewBoardService reviewBoardService;
-    
 
     public BoardController(ReviewBoardService reviewBoardService) {
         this.reviewBoardService = reviewBoardService;
     }
 
-    @GetMapping("/")
-    public String index(){
-        return "index";
-    }
+    /*
+     * @GetMapping("/")
+     * public String index(){
+     * return "index";
+     * }
+     */
 
     @GetMapping("/board/save")
-    public String saveForm() {return "save";}
+    public String saveForm() {
+        return "save";
+    }
 
     @PostMapping("/board/save")
-    public String reviewBoardPost(ReviewBoard reviewBoard,MultipartFile file)throws Exception{
+    public String reviewBoardPost(ReviewBoard reviewBoard, MultipartFile file) throws Exception {
         reviewBoardService.save(reviewBoard);
         return "redirect:/";
     }
 
     @GetMapping("/board/")
-    public String findAll(Model model){
-        //db전체 게시글 데이터를 가져와서 보여줌
+    public String findAll(Model model) {
+        // db전체 게시글 데이터를 가져와서 보여줌
         List<ReviewBoard> boardList = reviewBoardService.findAll();
         model.addAttribute("boardAllList", boardList);
         return "list";
     }
 
     @GetMapping("/board/{id}")
-    public String findById(@PathVariable Long id,Model model){
+    public String findById(@PathVariable Long id, Model model) {
         // 게시글의 조회수를 하나 올리고 게시글 데이터를 가져와서 detail.html에 출력
         reviewBoardService.updateCount(id);
         Optional<ReviewBoard> reviewBoard = reviewBoardService.findOne(id);
@@ -55,25 +58,23 @@ public class BoardController {
     }
 
     @GetMapping("/board/update/{id}")
-    public String updateForm(@PathVariable Long id,Model model){
+    public String updateForm(@PathVariable Long id, Model model) {
         Optional<ReviewBoard> reviewBoard = reviewBoardService.findOne(id);
         model.addAttribute("boardUpdate", reviewBoard.get());
         return "update";
     }
 
     @PostMapping("/board/update")
-    public String update(@ModelAttribute ReviewBoard reviewBoard,Model model){
+    public String update(@ModelAttribute ReviewBoard reviewBoard, Model model) {
         reviewBoardService.update(reviewBoard);
         model.addAttribute("board", reviewBoard);
         return "detail";
     }
 
     @GetMapping("/board/delete/{id}")
-    public String delete(@PathVariable Long id){
+    public String delete(@PathVariable Long id) {
         reviewBoardService.delete(id);
         return "redirect:/board/";
-    } 
+    }
 
-    
-    
 }
