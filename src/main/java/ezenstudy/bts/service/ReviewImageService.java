cae6edd656975046619566588edc0fe9274dfa0a
Zesting <pp950209@naver.com>
2023-05-01 18:07:56 +0900

    2023.05.01_total_commit

--- src/main/java/ezenstudy/bts/service/ReviewImageService.java
+++ src/main/java/ezenstudy/bts/service/ReviewImageService.java
@@ -8,35 +8,34 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import ezenstudy.bts.DTO.ReviewBoardDTO;
-import ezenstudy.bts.repository.ReviewImageRepository;
 
 @Service
 public class ReviewImageService {
     // private ReviewImageRepository reviewImageRepository;
 
     // public ReviewImageService(ReviewImageRepository reviewImageRepository) {
-    //     this.reviewImageRepository = reviewImageRepository;
+    // this.reviewImageRepository = reviewImageRepository;
     // }
 
     public void save(ReviewBoardDTO reviewBoardDTO) throws IOException {
 
-        if(reviewBoardDTO.getFile().isEmpty()){
-            
+        if (reviewBoardDTO.getFile().isEmpty()) {
+
         }
 
         // 파일경로 설정 (user.dir = 현재 디렉토리)
         String filePath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
         // 랜덤 파일명(중복안되게)
         UUID uuid = UUID.randomUUID();
-        for(MultipartFile file : reviewBoardDTO.getFile()){
-        // 저장될 파일 이름
-        String fileName = uuid + "_" + file.getOriginalFilename();
-        // 파일 저장 경로,이름
-        File saveFile = new File(filePath, fileName);
-        // 파일 변환 후 저장
-        file.transferTo(saveFile);
-        reviewBoardDTO.setFileName(fileName);
-        reviewBoardDTO.setFilePath("/files/" + filePath);
+        for (MultipartFile file : reviewBoardDTO.getFile()) {
+            // 저장될 파일 이름
+            String fileName = uuid + "_" + file.getOriginalFilename();
+            // 파일 저장 경로,이름
+            File saveFile = new File(filePath, fileName);
+            // 파일 변환 후 저장
+            file.transferTo(saveFile);
+            reviewBoardDTO.setFileName(fileName);
+            reviewBoardDTO.setFilePath("/files/" + filePath);
         }
 
     }
